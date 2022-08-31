e82f6eb04642d4bd2626688875111ef370ae42a8
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-08-31 12:38:53 +0100

    feat: check user answer is correct and send feedback

--- love-maths/src/assets/js/script.js
+++ love-maths/src/assets/js/script.js
@@ -1,12 +1,15 @@
 let buttons = document.querySelectorAll("button");
+const questionArea = document.getElementById("question-area");
 
 for (let button of buttons) {
   let gameType;
 
   button.addEventListener("click", () => {
-    button.getAttribute("data-type") !== "submit"
-      ? ((gameType = button.getAttribute("data-type")), runGame(gameType))
-      : alert("You clicked submit");
+    if (button.getAttribute("data-type") !== "submit") {
+      (gameType = button.getAttribute("data-type")), runGame(gameType);
+    } else {
+      checkAnswer();
+    }
   });
 }
 
@@ -30,19 +33,47 @@ function runGame(gameType) {
   }
 }
 
-function checkAnswer() {}
+/**
+ * Check user answer against the first returned
+ * element in the calculateAnswer array
+ */
+function checkAnswer() {
+  let userAnswer = parseInt(questionArea.children[4].value);
+  let calculateAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculateAnswer[0];
 
-function calculateScore() {}
+  isCorrect
+    ? alert("Hey your'e answer is correct")
+    : alert(`You answered incorrectly. Please try again`);
+
+  // runGame(calculateAnswer[1]);
+}
+
+/**
+ * Get the operands (numbers) & operator (+, -, etc)
+ * from DOM, and returns correct answer
+ */
+function calculateCorrectAnswer() {
+  let operand1 = parseInt(questionArea.children[0].textContent);
+  let operand2 = parseInt(questionArea.children[2].textContent);
+  let operator = questionArea.children[1].textContent;
+
+  if (operator === "+") {
+    return [operand1 + operand2, "addition"];
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw Error(`Unknown game type: ${gameType}. Aborting!`);
+  }
+}
 
 function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
 function displayAdditionQuestion(operand1, operand2) {
-  let questionArea = document.getElementById("question-area");
   questionArea.children[0].textContent = operand1;
-  questionArea.children[1].textContent = "+";
   questionArea.children[2].textContent = operand2;
+  questionArea.children[1].textContent = "+";
 }
 
 function displaySubtractionQuestion() {}
