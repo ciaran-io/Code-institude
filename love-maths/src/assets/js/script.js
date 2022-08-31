ce49c027ff5e3b4eb667bc94ee1a40ab66833903
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-08-31 10:08:33 +0100

    feat: add addition question & answer

--- love-maths/src/assets/js/script.js
+++ love-maths/src/assets/js/script.js
@@ -5,20 +5,29 @@ for (let button of buttons) {
 
   button.addEventListener("click", () => {
     button.getAttribute("data-type") !== "submit"
-      ? ((gameType = button.getAttribute("data-type")),
-        alert(`You clicked ${gameType}`))
+      ? ((gameType = button.getAttribute("data-type")), runGame(gameType))
       : alert("You clicked submit");
   });
 }
+
+// Start game with addition
+runGame("addition");
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
   // Creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw Error(`Unknown game type: ${gameType}. Aborting!`);
+  }
 }
 
 function checkAnswer() {}
@@ -29,7 +38,12 @@ function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+  let questionArea = document.getElementById("question-area");
+  questionArea.children[0].textContent = operand1;
+  questionArea.children[1].textContent = "+";
+  questionArea.children[2].textContent = operand2;
+}
 
 function displaySubtractionQuestion() {}
 
