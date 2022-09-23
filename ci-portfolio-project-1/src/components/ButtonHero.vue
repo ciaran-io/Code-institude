e59719bcaff29cb6c812cda25b9db965af9e7948
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-09-23 07:59:23 +0100

    feat: add button components

---
+++ ci-portfolio-project-1/src/components/ButtonHero.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  defineProps({
+    pageLink: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <nuxt-link
+    class="button button-primary button-rounded-hero button-padding-hero"
+    :to="`${pageLink}`"
+    >{{ text }}
+  </nuxt-link>
+</template>
