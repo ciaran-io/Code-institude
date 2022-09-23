6b78c94db49f0d29459bde2a286f293660691fe9
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-09-23 08:22:30 +0100

    feat: update tailwind config.
    1. Extends tailwind base colors for usage alongside tailwind

--- ci-portfolio-project-1/tailwind.config.js
+++ ci-portfolio-project-1/tailwind.config.js
@@ -8,7 +8,41 @@ module.exports = {
     './src/nuxt.config.{js,ts}',
   ],
   theme: {
-    extend: {},
+    container: {
+      padding: '2rem',
+    },
+    extend: {
+      backgroundColor: {
+        'theme-base': 'var(--theme-base)',
+        'theme-base-100': 'var(--theme-base-100)',
+        'theme-base-200': 'var(--theme-base-200)',
+
+        'theme-gray': 'var(--theme-gray)',
+        'theme-gray-100': 'var(--theme-gray-100)',
+        'theme-gray-500': 'var(--theme-gray-500)',
+        'theme-gray-600': 'var(--theme-gray-600)',
+      },
+      colors: {
+        'theme-base': 'var(--theme-base)',
+        'theme-base-100': 'var(--theme-base-100)',
+        'theme-base-200': 'var(--theme-base-200)',
+
+        'theme-gray': 'var(--theme-gray)',
+        'theme-gray-100': 'var(--theme-gray-100)',
+        'theme-gray-500': 'var(--theme-gray-500)',
+        'theme-gray-600': 'var(--theme-gray-600)',
+      },
+      spacing: {
+        gutter: 'var(--gutter)',
+      },
+      borderRadius: {
+        base: 'var(--rounded-base)',
+      },
+      boxShadow: {
+        primary: 'var(--shadow-primary)',
+        'base-1': 'var(--shadow-shadow-base-1)',
+        'base-2': 'var(--shadow-shadow-base-2)',
+      },
+    },
   },
-  plugins: [],
 }
