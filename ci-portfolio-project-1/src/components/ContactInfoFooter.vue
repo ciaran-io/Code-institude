9a1f5973c57f86b40c63bfd2c8c37ec2ebda02dd
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-09-23 08:13:15 +0100

    feat: add contact information component

---
+++ ci-portfolio-project-1/src/components/ContactInfoFooter.vue
@@ -0,0 +1,54 @@
+<script setup>
+  defineProps({
+    contactInformation: {
+      type: Object,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <!-- contact information -->
+  <div class="md:row-span-2 md:justify-self-center">
+    <div aria-label="show collapsed content on mobile">
+      <TheMenuHeading heading="contact us">
+        <template #menuIcon>
+          <Icon
+            class="float-right h-5 w-5 text-theme-gray-500 transition-transform duration-150 md:!hidden"
+            aria-hidden="true"
+            name="gridicons:chevron-down"
+          />
+        </template>
+      </TheMenuHeading>
+    </div>
+
+    <div class="hide-on-mobile">
+      <h3>Get in touch by email or phone</h3>
+      <template
+        v-for="(information, value) in contactInformation"
+        :key="information"
+      >
+        <div class="mt-3">
+          <div v-if="value !== 'opening hours'">
+            <h4>{{ value }}</h4>
+            <p>{{ information }}</p>
+          </div>
+          <div v-else>
+            <h4>{{ value }}</h4>
+            <p>{{ information.day }} {{ information.hours }}</p>
+          </div>
+        </div>
+      </template>
+
+      <div class="mt-6 space-x-4">
+        <TheSocialIconBlock />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+  h4 {
+    @apply font-medium capitalize;
+  }
+</style>
