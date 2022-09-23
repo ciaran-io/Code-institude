802604ff53fc0969ebad625dcb43ff40739ebb44
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-09-23 08:15:13 +0100

    feat: add social links component.

---
+++ ci-portfolio-project-1/src/components/TheSocialIconBlock.vue
@@ -0,0 +1,24 @@
+<template>
+  <nuxt-link
+    to="https://www.facebook.com/"
+    target="_blank"
+  >
+    <Icon
+      class="w-8 h-8"
+      name="entypo-social:facebook"
+  /></nuxt-link>
+  <nuxt-link
+    to="https://www.instgram.com/"
+    target="_blank"
+  >
+    <Icon
+      class="h-9 w-9"
+      name="ph:instagram-logo-fill"
+  /></nuxt-link>
+</template>
+
+<style lang="postcss" scoped>
+  svg {
+    @apply text-theme-gray-500;
+  }
+</style>
