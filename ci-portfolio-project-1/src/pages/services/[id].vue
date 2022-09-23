bb07e64cb1675173efa87878a91c0f4bec79a292
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-09-23 07:58:07 +0100

    feat: update base pages
    1. Add home page content (services, reviews, book service).
    2. Add dynamic services page.
    3. Add contact page

--- ci-portfolio-project-1/src/pages/services/[id].vue
+++ ci-portfolio-project-1/src/pages/services/[id].vue
@@ -1,9 +1,48 @@
-<script lang="ts" setup></script>
+<script setup>
+  definePageMeta({
+    layout: 'services',
+  })
+
+  const store = useStore()
+  const { services, serviceNames } = store.value
+  const serviceIndex = ref(0)
+
+  function changeService(index) {
+    serviceIndex.value = index
+  }
+</script>
 
 <template>
-  <div>
-    Page: foo
+  <div class="container max-w-5xl mx-auto mt-16 space-y-12">
+    <div>
+      <img
+        class="h-96 w-full rounded-md"
+        src="/images/aromatherapy.jpg"
+        alt=""
+      />
+    </div>
+    <main>
+      <section class="space-y-20">
+        <h1 class="text-theme-gray-100 text-4xl text-center">Our Services</h1>
+
+        <ServicesList
+          :services="services"
+          :service-id="serviceIndex"
+          :service-names="serviceNames"
+          @update:selected="changeService"
+        />
+
+        <ServicesCardDetails
+          :services="services"
+          :service-id="serviceIndex"
+        />
+      </section>
+    </main>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="postcss" >
+  select {
+    @apply !bg-theme-base-200;
+  }
+</style>
