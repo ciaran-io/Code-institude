fee84a48c0de4af473274220b115b0781b35097c
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-30 12:02:29 +0000

    feat: connect to google API, retrieve data
    1. Retrieve data from google sheets api.

---
+++ love-sandwiches/run.py
@@ -0,0 +1,23 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+# List of scopes for the API can access
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+# File name of the credentials
+CREDS = Credentials.from_service_account_file('creds.json')
+# Create a copy of the credentials with the specified scopes
+SCOPE_CREDS = CREDS.with_scopes(SCOPE)
+# Authorize the client sheet
+GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+# Get the instance of the Spreadsheet
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+# Access the specific sheet
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
