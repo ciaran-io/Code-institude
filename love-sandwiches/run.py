1ba3ea3d26875ff5133819f49b6a50dc4db15377
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-12-01 12:05:06 +0000

    refactor: reduce code duplication
    1. Pass worksheet name as argument to update specified worksheet.

--- love-sandwiches/run.py
+++ love-sandwiches/run.py
@@ -68,26 +68,17 @@ def validate_data(values: list):
     return True
 
 
-def update_sales_workSheet(data: list):
-    """Update sales worksheet.
+def update_worksheet(data: list, worksheet: str):
+    """Update worksheet
 
-    Add new row with list data provided.
+    Receives a list of integers to be inserted into a worksheet.\n
+    Updates the relevant worksheet with the data provided
     """
-    print("Updating sales work sheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
 
-
-def update_surplus_worksheet(data: list):
-    """Update surplus worksheet
-
-    Add new row with list data provided.
-    """
-    print("Updating surplus work sheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
+    print(f"Updating {worksheet} work sheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully.\n")
 
 
 def calculate_surplus_data(sales_row: list):
@@ -115,11 +106,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_workSheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
-
-    # print(f"surplus: {new_surplus_data}")
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation")
